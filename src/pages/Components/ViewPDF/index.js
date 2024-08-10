106bbe79578eb3f4b1a4cdc5a40e7b3c58fb2b46
huangtian1_v <1246562955@qq.com>
2024-08-10 23:43:20 +0800

    0810优化

--- src/pages/Components/ViewPDF/index.js
+++ src/pages/Components/ViewPDF/index.js
@@ -1,9 +1,18 @@
+/*
+ * @Author: huangtian1_v 1246562955@qq.com
+ * @Date: 2024-08-10 17:04:12
+ * @LastEditors: huangtian1_v 1246562955@qq.com
+ * @LastEditTime: 2024-08-10 23:41:56
+ * @FilePath: \yellowsky-blog\src\pages\Components\ViewPDF\index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import FilePDF from "./FilePDF";
 import "./FilePDF.scss";
 const ViewPdf = () => {
+  const filePath = require("@/assets/others/2024.pdf");
   return (
     <div className="viewPdfStyle">
-      <FilePDF filePath="/黄天2024.pdf" />
+      <FilePDF filePath={filePath} />
     </div>
   );
 };
