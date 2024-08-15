92dd90b1715dc875802e6d5b76ea6a9b7884f8a4
huangtian1_v <1246562955@qq.com>
2024-08-15 16:38:21 +0800

    图片压缩打包优化

--- craco.config.js
+++ craco.config.js
@@ -2,7 +2,7 @@
  * @Author: huangtian1_v 1246562955@qq.com
  * @Date: 2024-08-08 16:50:10
  * @LastEditors: huangtian1_v 1246562955@qq.com
- * @LastEditTime: 2024-08-08 16:50:20
+ * @LastEditTime: 2024-08-15 16:27:51
  * @FilePath: \yellowsky-resume\craco.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,6 +11,23 @@ module.exports = {
   webpack: {
     alias: {
       "@": path.join(__dirname, "src"),
+    },
+    module: {
+      strictExportPresence: true,
+      rules: [
+        {
+          oneOf: [
+            {
+              test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+              loader: [
+                require.resolve("url-loader") +
+                  "?limit=10000&name=static/media/[name].[hash:8].[ext]",
+                "image-webpack-loader",
+              ],
+            },
+          ],
+        },
+      ],
     },
   },
 };
