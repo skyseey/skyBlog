616c413c351817b59a4b5a598248af7c82d9ea83
huangtian1_v <1246562955@qq.com>
2025-05-27 21:41:22 +0800

    0527

--- src/pages/Cp/MyDemo/components/Editor/index.js
+++ src/pages/Cp/MyDemo/components/Editor/index.js
@@ -2,29 +2,79 @@
  * @Author: huangtian1_v 1246562955@qq.com
  * @Date: 2025-04-05 21:54:21
  * @LastEditors: huangtian1_v 1246562955@qq.com
- * @LastEditTime: 2025-05-26 18:09:21
+ * @LastEditTime: 2025-05-27 21:36:31
  * @FilePath: \yellowsky-blog\src\pages\Cp\MyDemo\components\TodoAnimation\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import Style from "./index.module.scss";
-import ReactQuill from "react-quill";
-import { useState } from "react";
 import "react-quill/dist/quill.snow.css"; // 引入样式
+import "./custom-styles.scss";
+import ReactQuill from "react-quill";
+import { useRef, useState } from "react";
+
+// 工具栏配置
+const modules = {
+  // toolbar: [
+  //   ["bold", "italic", "underline", "strike"],
+  //   ["blockquote", "code-block"],
+  //   [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  //   [{ list: "ordered" }, { list: "bullet" }],
+  //   ["link", "image", "video"],
+  //   ["clean"],
+  // ],
+  toolbar: {
+    container: [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      ["blockquote", "code-block"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image", "video"],
+      ["clean"],
+    ],
+  },
+};
+
+const formats = [
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "code-block",
+  "list",
+  "bullet",
+  "link",
+  "image",
+  "video",
+];
+
 const Edite = () => {
   const [content, setContent] = useState("");
+  const quillRef = useRef(null);
+  const onChange = (value) => {
+    console.log(value);
+    setContent(value);
+  };
   return (
     <div className={Style.ReEditorStyle}>
       <ReactQuill
+        ref={quillRef}
         theme="snow" // 主题风格
         value={content}
-        onChange={setContent}
-        modules={{
-          toolbar: [
-            ["bold", "italic", "underline"],
-            [{ list: "ordered" }, { list: "bullet" }],
-          ],
-        }}
+        onChange={onChange}
+        modules={modules}
+        formats={formats}
       />
+      <div className="mt-4 border rounded-lg p-4">
+        <h2 className="text-xl font-bold">预览</h2>
+        <div dangerouslySetInnerHTML={{ __html: content }} />
+      </div>
     </div>
   );
 };
--- src/pages/Resume/todolist.js
+++ src/pages/Resume/todolist.js
@@ -2,11 +2,11 @@
  * @Author: huangtian1_v 1246562955@qq.com
  * @Date: 2024-08-08 14:36:07
  * @LastEditors: huangtian1_v 1246562955@qq.com
- * @LastEditTime: 2025-05-24 16:43:16
+ * @LastEditTime: 2025-05-27 21:07:34
  * @FilePath: \yellowsky-blog\src\pages\Resume\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 // moks
 let resorceList = [];
@@ -14,50 +14,66 @@ for (let i = 0; i < 1000; i++) {
   resorceList.push(i);
 }
 
-export default function Resume() {
+const useTodo = () => {
   const [value, setValue] = useState("");
-  const [list, setList] = useState(resorceList);
+  const [list, setList] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(null);
-  const [page, setPage] = useState(1);
-
-  useEffect(() => {
-    let currentlist = resorceList.slice((page - 1) * 10, page * 10);
-    setList(currentlist);
-  }, [page]);
-
-  const add = () => {
+  const add = (value) => {
+    setValue("");
     setList([...list, value]);
   };
-  const delFn = (item, index) => {
+  const delFn = (index) => {
     list.splice(index, 1);
     setList([...list]);
   };
-  const editFn = (item, index) => {
+  const editFn = (index) => {
     setCurrentIndex(index);
   };
   const agreen = (item, index) => {
     list[index] = item;
     setList([...list]);
   };
-
-  // 分页函数
-  const pageFn = (value) => {
-    setPage(value);
+  return {
+    value,
+    setValue,
+    currentIndex,
+    list,
+    setList,
+    add,
+    delFn,
+    editFn,
+    agreen,
   };
+};
+
+export default function Resume() {
+  const { value, setValue, currentIndex, list, add, delFn, editFn, agreen } =
+    useTodo();
+  // const [page, setPage] = useState(1);
+
+  // useEffect(() => {
+  //   let currentlist = resorceList.slice((page - 1) * 10, page * 10);
+  //   setList(currentlist);
+  // }, [page]);
+
+  // // 分页函数
+  // const pageFn = (value) => {
+  //   setPage(value);
+  // };
   return (
     <div className="content">
       <input value={value} onChange={(e) => setValue(e.target.value)}></input>
       <button
         onClick={() => {
-          add();
+          add(value);
         }}
       >
         add
       </button>
       {list.map((item, index) => {
         return (
-          <div key={index}>
-            {currentIndex == index ? (
+          <div style={{ display: "flex", gap: "10px" }} key={index}>
+            {currentIndex === index ? (
               <div>
                 <input
                   value={item}
@@ -67,7 +83,7 @@ export default function Resume() {
                 ></input>
                 <button
                   onClick={() => {
-                    setCurrentIndex(null);
+                    editFn(null);
                   }}
                 >
                   agreen
@@ -79,14 +95,14 @@ export default function Resume() {
 
             <button
               onClick={() => {
-                delFn(item, index);
+                delFn(index);
               }}
             >
               del
             </button>
             <button
               onClick={() => {
-                editFn(item, index);
+                editFn(index);
               }}
             >
               edit
@@ -96,7 +112,7 @@ export default function Resume() {
       })}
 
       {/* 分页 */}
-      <input value={page} onChange={(e) => pageFn(e.target.value)}></input>
+      {/* <input value={page} onChange={(e) => pageFn(e.target.value)}></input> */}
     </div>
   );
 }
